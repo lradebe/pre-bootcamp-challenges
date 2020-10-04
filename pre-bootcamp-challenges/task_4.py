9ee9da923258a95ef36714a15dca7c5732fcbde4
lradebe <60396576+lradebe@users.noreply.github.com>
2020-10-04 08:52:58 +0200

    Update task_4.py

--- pre-bootcamp-challenges/task_4.py
+++ pre-bootcamp-challenges/task_4.py
@@ -4,7 +4,13 @@ b = int (b)
 c = a + b
 c = str (c)
 
-if a == 3 or b == 3 and c.find ('3') == -1:
+if a == 3 and c.find ('3') == -1:
+
     print ("FALSE")
+
+elif b == 3 and c.find ('3') == -1:
+
+    print ('False')
+
 else:
     print ("TRUE!")
