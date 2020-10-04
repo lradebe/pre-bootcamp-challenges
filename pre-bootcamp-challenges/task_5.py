7dd634b2061ba2251e99288d56ef1668b6f682c7
lradebe <60396576+lradebe@users.noreply.github.com>
2020-10-04 08:58:28 +0200

    Update task_5.py

--- pre-bootcamp-challenges/task_5.py
+++ pre-bootcamp-challenges/task_5.py
@@ -2,9 +2,13 @@
 a = float (input ('Enter first side: '))
 b = float (input ('Enter second side: '))
 c = float (input ("Enter base side: "))
+
 import math
-#s is semi-parameter
+
 s = (a + b + c)/2
+
 Area = math.sqrt (s*(s-a)*(s-b)*(s-c))
+
 Area = str (Area)
+
 print ("The area of the triangle is: " + Area)
