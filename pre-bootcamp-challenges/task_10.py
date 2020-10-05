187741c55e4785217b66f5d9b3ac7601f99e71bc
Lwazi <lwaziradebe100@gmail.com>
2020-10-05 08:38:59 +0200

    Added comments

--- pre-bootcamp-challenges/task_10.py
+++ pre-bootcamp-challenges/task_10.py
@@ -1,5 +1,15 @@
+#defining a function that takes a parameter
+#and returns the vowels in it
 def vowels (Words):
 
+#assigning capital and small letter vowels to their variable
   vowels = "AEIOUaeiou"
+
+#print
+#character for characrer in parameter if character is found in
+#variable vowels
+
   print ([char for char in Words if char in vowels])
+
+#calling out function
 vowels ('JCOle VS. KendrIck LamAr')
