faae54caa205965798f13a70c908c9d6ac1602e5
Lwazi <lwaziradebe100@gmail.com>
2020-10-05 08:35:04 +0200

    Added comments

--- pre-bootcamp-challenges/task_9.py
+++ pre-bootcamp-challenges/task_9.py
@@ -1,10 +1,17 @@
+#the range of 0 to 1000 is assigned to numbers
 numbers = range (0, 1000)
+#we start with no multiples
 multiples = 0
 
+
 for i in numbers:
-#if there is no remainder when diving 3/5 from i
+#if there is no remainder when diving 3 or 5 from i
   if i % 3 == 0 or i % 5 == 0:
 #then add whatever value found in multiples with both values of i
     multiples += i
-#when i has reached 1000, max multiples have been reached and added togeth>
+#when i has reached 1000
+#max multiples have been reached and added together
+
+
+#show the sum of all the multiples of 3 and 5 below 1000
 print (multiples)
