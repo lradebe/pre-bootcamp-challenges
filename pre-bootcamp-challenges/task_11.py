71bc90f8306ba4c9950fe5c9d3e1faf40e910693
Lwazi <lwaziradebe100@gmail.com>
2020-10-05 08:43:59 +0200

    Added comments

--- pre-bootcamp-challenges/task_11.py
+++ pre-bootcamp-challenges/task_11.py
@@ -1,10 +1,20 @@
+#defining a function that takes two words from user
+#and must return common letters between them
 def common ():
 
   wrd1, wrd2 = input ("First word: "), input ('Second word: ')
+
+#convert both inputs into sets
   w1 = set (wrd1)
   w2 = set (wrd2)
+
+#once they're sets use & to take common letters
+#between both sets
+#turn whatever common letters found into a list
   lst = list (w1 & w2)
 
+#print common letters in a list format
   print ('Common letters: {}'.format (lst))
 
+#call out function
 common ()
