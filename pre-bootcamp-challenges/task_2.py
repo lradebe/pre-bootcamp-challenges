98011c73857dc7089b0f0ed442ac6bbafa9013a3
Lwazi <lwaziradebe100@gmail.com>
2020-10-05 07:15:13 +0200

    Added comments

--- pre-bootcamp-challenges/task_2.py
+++ pre-bootcamp-challenges/task_2.py
@@ -1,10 +1,9 @@
-x = 1 + 1*2
-y = (1+1)  * 2
-z = 1 + (1 * 2)
-a = 1 + 1 * 2/2
-b = (1 + 1 * 2)/2
-
-print (x)
+x = 1 + 1*2 #assigning to x
+y = (1+1)  * 2 #assigning to y
+z = 1 + (1 * 2) #assigning to z
+a = 1 + 1 * 2/2 #assigning to a
+b = (1 + 1 * 2)/2 #assigning to b
+print (x)                              
 print (y)
 print (z)
 print (a)
